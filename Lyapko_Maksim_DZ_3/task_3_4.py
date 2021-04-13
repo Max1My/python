e61c72ecf24346c7bb3d94f4427d53b4b96faeb6
maximy <maximy.pro@gmail.com>
2021-04-13 13:52:56 +0300

    task_3_4.py is done

--- Lyapko_Maksim_DZ_3/task_3_4.py
+++ Lyapko_Maksim_DZ_3/task_3_4.py
@@ -1,17 +1,23 @@
-from random import choice
+from random import randrange
 
 def get_jokes(num):
-    jokes = num
-    random_list = []
-    while jokes > 0:
-        random_jokes = choice(nouns) + ' ' + choice(adverbs) + ' ' + choice(adjectives)
-        random_list.append(random_jokes)
-        jokes -= 1
-    print(random_list)
+    while num > 0:
+        random_joke1 = randrange(len(nouns))
+        random_joke2 = randrange(len(adverbs))
+        random_joke3 = randrange(len(adjectives))
+        joke = nouns[random_joke1] + ' ' + adverbs[random_joke2] + ' ' + adjectives[random_joke3]
+        jokes_list.append(joke)
+        num -= 1
     return num
 
 nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
-get_jokes(2, flag = true)
+jokes_list = []
+joke = get_jokes(2)
+while all(jokes_list[i] == jokes_list[i+1] for i in range(len(jokes_list)-1)):
+    jokes_list =[]
+    get_jokes(2)
+else:
+    print(jokes_list)
