c88c389b70f07542147782d8922ec63ade30a5f6
maximy <maximy.pro@gmail.com>
2021-04-16 14:49:37 +0300

    task_3_5 is done

--- Lyapko_Maksim_DZ_3/task_3_5.py
+++ Lyapko_Maksim_DZ_3/task_3_5.py
@@ -1,23 +1,39 @@
 from random import randrange
 
-def get_jokes(num):
+def get_jokes(num): #сложение в одну фразу
     while num > 0:
-        random_joke1 = randrange(len(nouns))
-        random_joke2 = randrange(len(adverbs))
-        random_joke3 = randrange(len(adjectives))
-        joke = nouns[random_joke1] + ' ' + adverbs[random_joke2] + ' ' + adjectives[random_joke3]
+        joke = []
+        joke.append(sort_nouns())
+        joke.append(sort_adverbs())
+        joke.append(sort_adjectives())
         jokes_list.append(joke)
         num -= 1
-    return num
+    return jokes_list
+
+def sort_nouns(): #генерация nouns
+    index_random_nouns = randrange(len(nouns))
+    random_nouns = nouns[index_random_nouns]
+    nouns.pop(index_random_nouns)
+    return random_nouns
+def sort_adverbs(): #генерация adverbs
+    index_random_adverbs = randrange(len(adverbs))
+    random_adverbs = adverbs[index_random_adverbs]
+    adverbs.pop(index_random_adverbs)
+    return random_adverbs
+def sort_adjectives(): #генерация adjectives
+    index_random_adjectives = randrange(len(adjectives))
+    random_adjectives = adjectives[index_random_adjectives]
+    adjectives.pop(index_random_adjectives)
+    return random_adjectives
 
 nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
 jokes_list = []
-joke = get_jokes(2)
-while all(jokes_list[i] == jokes_list[i+1] for i in range(len(jokes_list)-1)):
-    jokes_list =[]
-    get_jokes(2)
+num_jokes = int(input('Количество шуток: '))
+if num_jokes > 5:
+    print('Максимальное количество шуток - 5')
 else:
-    print(jokes_list)
+    jokes_list = get_jokes(num_jokes)
+print(' '.join(map(str,jokes_list)))
