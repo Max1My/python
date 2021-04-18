ccb892d4af820e17f760fc0b61e6e379e2914c17
Max1My <57528691+Max1My@users.noreply.github.com>
2021-04-18 21:40:23 +0300

    Delete task_2_2.py

--- Lyapko_Maksim_DZ_2/task_2_2.py
+++
@@ -1,9 +0,0 @@
-lst1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-lst1.insert(1,'"')
-lst1.insert(3,'"')
-lst1.insert(5,'"')
-lst1.insert(7,'"')
-lst1.insert(12,'"')
-lst1.insert(14,'"')
-lst1 = ' '.join(lst1)
-print(lst1)
