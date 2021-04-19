20a3aa23c8be735d6c8cf9293cabf479c0645ddc
Max1My <57528691+Max1My@users.noreply.github.com>
2021-04-19 08:54:04 +0300

    Delete Lyapko_Maksim_DZ_4 directory

--- Lyapko_Maksim_DZ_4/task_4_2.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-response.encoding = 'utf-8' # Optional: requests infers this internally
-print(type(response))
-print(dir(response))
