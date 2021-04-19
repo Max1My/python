4d11ad96f9e6a5d16c14f01c58277eb716c31cda
Max1My <57528691+Max1My@users.noreply.github.com>
2021-04-19 08:58:31 +0300

    Delete Lyapko_Maksim_DZ_2 directory

--- Lyapko_Maksim_DZ_2/task_2_4.py
+++
@@ -1,8 +0,0 @@
-lst_text = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for item in lst_text:
-    message = "".join(item)
-    message = message.split(' ')
-    message = message.pop()
-    message = message.title()
-    print('Привет ,', message)
-
