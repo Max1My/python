b3ccb5cf84b01ab559f356079874fbaffda28e1f
maximy <maximy.pro@gmail.com>
2021-04-21 10:12:29 +0300

    task_4_2 done

--- Lyapko_Maksim_DZ_4/task_4_2.py
+++ Lyapko_Maksim_DZ_4/task_4_2.py
@@ -1,9 +1,8 @@
-from typing import Any
-
 import requests
 import xml.etree.ElementTree as et
+from decimal import Decimal
 
-
+# Конвертация
 def currency_rates(valute):
     select_currency = {k: v for k, v in get_currency.items() if k == valute}
     if valute not in select_currency:
@@ -13,26 +12,25 @@ def currency_rates(valute):
             select_currency[value] = select_currency[value].replace(',', '.')
             print(select_currency)
         valute1 = float(select_currency[value])
-        valute1 = round(rubble / valute1, 2)
-        valute1 = str(valute1)
-        print(valute1 + ' $')
+        valute1 = (rubble / valute1)
+        valute1 = Decimal(valute1)
+        print(valute1.quantize(Decimal("1.00")))
 
-
-
+# Адрес api метода для запроса get
 url = 'http://www.cbr.ru/scripts/XML_daily.asp'
-
+# Отправляем get request (запрос GET)
 response = requests.get(url)
 tree = et.ElementTree(et.fromstring(response.text))
 root = tree.getroot()
-
+# Создаем словарь из полученного
 get_currency = {}
 for valute in root.findall('Valute'):
     charcode = valute.find('CharCode').text
     valute_count = valute.find('Value').text
     get_currency.update({charcode: valute_count})
-
+# Выбор валюты
 select_row = {}
 rubble = int(input("Введите сумму рублей которые хотите сконвертировать: "))
 select_valute = input('Введите валюту: ')
-select_valute.upper()
+select_valute = select_valute.upper()
 currency_rates(select_valute)
